c385c10e1ac323def908aeac1b07a89b1aa162f8
meikidd <meikidd@gmail.com>
2019-01-12 15:10:52 +0800

    update shadowing

---
+++ shadowing/precache-manifest.31124df2b0abd71495afc51a73c2734e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce71a64a9dd2e9e8ec3e",
+    "url": "/shadowing/static/js/runtime~main.ce71a64a.js"
+  },
+  {
+    "revision": "bc66e8b8b6adda33c69a",
+    "url": "/shadowing/static/js/main.bc66e8b8.chunk.js"
+  },
+  {
+    "revision": "f8344f96a42ba4e3c936",
+    "url": "/shadowing/static/js/1.f8344f96.chunk.js"
+  },
+  {
+    "revision": "bc66e8b8b6adda33c69a",
+    "url": "/shadowing/static/css/main.ff7f9827.chunk.css"
+  },
+  {
+    "revision": "56bfe361824b42c5571abe8611527cbf",
+    "url": "/shadowing/index.html"
+  }
+];
--- shadowing/service-worker.js
+++ shadowing/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shadowing/precache-manifest.bfce18b898200f48ee026fde49f54d21.js"
+  "/shadowing/precache-manifest.31124df2b0abd71495afc51a73c2734e.js"
 );
 
 workbox.clientsClaim();
